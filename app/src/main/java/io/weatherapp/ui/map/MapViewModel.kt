3b5a0a65cef34f94ee0ae5eea9e0a96e93f8dc32
Tatyana Nikulina <tatyananikulina2206@gmail.com>
2020-11-14 19:16:12 +0200

    Added nav graph

---
+++ app/src/main/java/io/weatherapp/ui/map/MapViewModel.kt
@@ -0,0 +1,7 @@
+package io.weatherapp.ui.map
+
+import androidx.lifecycle.ViewModel
+
+class MapViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/io/weatherapp/ui/weather/WeatherViewModel.kt
@@ -0,0 +1,7 @@
+package io.weatherapp.ui.weather
+
+import androidx.lifecycle.ViewModel
+
+class WeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
